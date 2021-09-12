9e97686e1bc4ea73d265f80a55a647c8f4c36e9a
Shihab Chowdhury <90544016+Shihab-Ch0wdhury@users.noreply.github.com>
2021-09-12 11:57:45 +0600

    Assignment

---
+++ HTML Assignment/HTML/js/script.js
@@ -0,0 +1,6 @@
+function changetext() {
+    document.getElementById('ortext').innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus dolorum ab quasi neque et. Ipsum placeat dolorem ullam autem facere voluptatum voluptas a suscipit incidunt voluptatibus sed eaque repudiandae, ducimus voluptatem ipsam neque dolore magni? Reprehenderit voluptate natus, dolorum doloremque, tempora hic deleniti illo doloribus aut magnam nemo consequatur qui, laboriosam debitis? Praesentium illo natus accusantium facilis id rerum dignissimos earum iure quaerat ex mollitia, aspernatur, eius impedit odit! Inventore adipisci illum ut facere incidunt libero porro blanditiis velit maiores reiciendis ab aliquam a nulla fuga laborum debitis, impedit facilis beatae. Laborum dolorem itaque aut, vero sequi temporibus iusto!'
+    document.getElementById('ortext').style.color='blue'
+
+
+}
